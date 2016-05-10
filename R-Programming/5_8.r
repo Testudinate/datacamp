f759333298e4ff3eeb48ff48db27e52bfe2748d9
Developer <klevtsovstanislav@gmial.com>
2016-05-11 00:58:28 +0300

    Create 5_8.r

---
+++ R-Programming/5_8.r
@@ -0,0 +1,28 @@
+You will often want to select an entire column, namely one specific variable from a
+data frame. If you want to select all elements of the variable diameter, for example, both of these will do the trick:
+
+planets_df[,3]
+planets_df[,"diameter"]
+However, there is a short-cut. If your columns have names, you can use the $ sign:
+
+planets_df$diameter
+
+Task:
+
+Use the $ sign to select the rings variable from planets_df. Store the vector that results as rings_vector.
+Print out rings_vector to see if you got it right.
+
+Scripts:
+
+# planets_df is pre-loaded in your workspace
+
+# Select the rings variable from planets_df
+rings_vector <- planets_df$rings
+  
+# Print out rings_vector
+rings_vector
+
+Result:
+> # Print out rings_vector
+> rings_vector
+[1] FALSE FALSE FALSE FALSE  TRUE  TRUE  TRUE  TRUE
