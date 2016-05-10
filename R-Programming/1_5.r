63c2ef2428e6256283abb6021a655e1b27981f62
Developer <klevtsovstanislav@gmial.com>
2016-05-10 08:58:12 +0300

    Create 1_5.r

---
+++ R-Programming/1_5.r
@@ -0,0 +1,23 @@
+Every tasty fruit basket needs oranges, so you decide to add six oranges. 
+As a data analyst, your reflex is to immediately create the variable my_oranges and assign 
+the value 6 to it. Next, you want to calculate how many pieces of fruit you have in total. 
+Since you have given meaningful names to these values, you can now code this in a clear way: 
+
+my_apples + my_oranges
+
+
+    Assign to my_oranges the value 6.
+    Add the variables my_apples and my_oranges and have R simply print the result.
+    Assign the result of adding my_apples and my_oranges to a new variable my_fruit.
+
+# Assign a value to the variables my_apples and my_oranges
+my_apples <- 5
+
+
+# Add these two variables together
+my_oranges <- 6
+
+# Create the variable my_fruit
+my_fruit <- my_apples + my_oranges
+
+my_fruit
