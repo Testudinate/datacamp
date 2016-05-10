a90ef3935b55a2036f7def3fdc4a79b39d9ab33e
Developer <klevtsovstanislav@gmial.com>
2016-05-10 09:02:30 +0300

    Create 2_1.r

---
+++ R-Programming/2_1.r
@@ -0,0 +1,12 @@
+Feeling lucky? You better, because this chapter takes you on a trip to the City of Sins, also known as Statisticians Paradise!
+
+Thanks to R and your new data-analytical skills, you will learn how to uplift your 
+performance at the tables and fire off your career as a professional gambler. 
+This chapter will show how you can easily keep track of your betting progress 
+and how you can do some simple analyses on past actions. Next stop, Vegas Baby… VEGAS!!
+
+Do you still remember what you have learned in the first chapter? Assign the value 
+"Go!" to the variable vegas. Remember: R is case sensitive!
+
+# Define the variable vegas
+vegas <- "Go!"
