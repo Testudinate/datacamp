8c98130b5da14b982b6c23ea9eedec8cadc6d567
Developer <klevtsovstanislav@gmial.com>
2018-03-22 16:07:05 +0300

    Update 30_pandas_filter.py

--- Data_Scientist_with_Python/30_pandas_filter.py
+++ Data_Scientist_with_Python/30_pandas_filter.py
@@ -1,6 +1,8 @@
 Driving right (2)
 
-The code in the previous example worked fine, but you actually unnecessarily created a new variable dr. You can achieve the same result without this intermediate variable. Put the code that computes dr straight into the square brackets that select observations from cars.
+The code in the previous example worked fine, but you actually unnecessarily created a new 
+variable dr. You can achieve the same result without this intermediate variable. 
+Put the code that computes dr straight into the square brackets that select observations from cars.
 Instructions
 Convert the code on the right to a one-liner that calculates the variable sel as before.
 
