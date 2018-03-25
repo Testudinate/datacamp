3af935042760d90b260ccf04455a339a8c123653
Developer <klevtsovstanislav@gmial.com>
2018-03-25 17:11:51 +0300

    Update 71_using_numpy_for_import_flat_files.py

--- Data_Scientist_with_Python/71_using_numpy_for_import_flat_files.py
+++ Data_Scientist_with_Python/71_using_numpy_for_import_flat_files.py
@@ -26,15 +26,12 @@ file = 'digits.csv'
 digits = np.loadtxt(file, delimiter=',')
 
 # Print datatype of digits
-print(digits)
+print(type(digits))
 
 # Select and reshape a row
-print(im)
-im = digits[3, 1:]
+im = digits[21, 1:]
 im_sq = np.reshape(im, (28, 28))
 
 # Plot reshaped data (matplotlib.pyplot already loaded as plt)
 plt.imshow(im_sq, cmap='Greys', interpolation='nearest')
 plt.show()
-    
-    
