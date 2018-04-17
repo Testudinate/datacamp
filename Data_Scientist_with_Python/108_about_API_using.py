9c554ecebf987b11fd22f162928449f2c6b5ecd1
Developer <klevtsovstanislav@gmial.com>
2018-04-17 03:44:03 +0300

    Create 108_about_API_using.py

---
+++ Data_Scientist_with_Python/108_about_API_using.py
@@ -0,0 +1,18 @@
+Pop quiz: What's an API?
+
+Which of the following statements about APIs is NOT true?
+Answer the question
+50xp
+Possible Answers
+Click or Press Ctrl+1 to focus
+
+    An API is a set of protocols and routines for building and interacting with software applications.
+    press 1
+    API is an acronym and is short for Application Program interface.
+    press 2
+    It is common to pull data from APIs in the JSON file format.
+    press 3
+    All APIs transmit data only in the JSON file format. ( IT IS FALSE )
+    press 4
+    An API is a bunch of code that allows two software programs to communicate with each other.
+    press 5
