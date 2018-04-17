44915859405c1906ff96f9cd634c46a9074ed1cd
Developer <klevtsovstanislav@gmial.com>
2018-04-17 03:29:46 +0300

    Create 106_json_get.py

---
+++ Data_Scientist_with_Python/106_json_get.py
@@ -0,0 +1,16 @@
+Pop quiz: What exactly is a JSON?
+
+Which of the following is NOT true of the JSON file format?
+
+Possible Answers
+
+    JSONs consist of key-value pairs.
+    press 1
+    JSONs are human-readable.
+    press 2
+    The JSON file format arose out of a growing need for real-time server-to-browser communication.
+    press 3
+    The function json.load() will load the JSON into Python as a list. (IT IS FALSE !!! )
+    press 4
+    The function json.load() will load the JSON into Python as a dictionary.
+    press 5
